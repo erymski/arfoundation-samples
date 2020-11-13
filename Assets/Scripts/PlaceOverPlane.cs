a8c8a5e87b7edc1de4a5621e1926833d70d86f0e
Eugene Rymski <eugene.rymski@autodesk.com>
2020-11-13 17:37:03 +0300

    Wheel placement with debugging info

---
+++ Assets/Scripts/PlaceOverPlane.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOverPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+
+    private ARRaycastManager _raycastManager;
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    void Awake()
+    {
+        _raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = Input.mousePosition;
+            touchPosition = new Vector2(mousePosition.x, mousePosition.y);
+            return true;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+#endif
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        var hits = new List<ARRaycastHit>();
+        if (_raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+//            Trace.TraceWarning("**** FOUND: " + hits.Count.ToString());
+            Debug.Log("**** FOUND: " + hits.Count.ToString());
+
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                //m_PlacedPrefab.tr
+
+//                Trace.TraceWarning("***** PLACE OBJECT ************");
+                Debug.LogWarning("***** PLACE OBJECT ************");
+
+                spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
